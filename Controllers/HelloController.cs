8ea61209a2436e175e3c687ea9d91024e72bb5e7
Raul-A-Flores <raul.alexander.floress@gmail.com>
2024-02-29 20:10:53 -0800

    crud

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -4,11 +4,47 @@ namespace cs_app.Controllers
 {
     public class HelloController
     {
+
         [HttpGet]
         [Route("")]
-        public string Index()
+        public string Index1()
         {
-            return "Hello World from Hello Controller";
+            return $"Hello !";
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public string Index(string name)
+        {
+            return $"Hello {name}!";
+        }
+
+        [HttpGet]
+        [Route("pizza/{topping}")]
+        public string PizzaParty(string topping)
+        {
+            return $"Whos ready for a {topping} Pizza!";
+        }
+
+
+        [HttpGet("about")]
+        public string About(){
+            return "About";
+        }
+
+        [HttpGet]
+        [Route("user/{name}/{location}/{age}")]
+        public string UserInfo(string name, string location, int age)
+        {
+            return $"{name}, aged {age}, is from {location}";
+        }
+
+
+
+        [HttpPost]
+        [Route("submission")]
+        public string FormSubmission(string formInput){
+            return "yep";
         }
     }
 }
